a014d7777bf92f768b3507745d1cb3d5389c62f1
Gulara Nuri <ramazanova.gulare@gmail.com>
2022-11-23 17:34:48 +0400

    editing url

--- src/Components/Register/Register.js
+++ src/Components/Register/Register.js
@@ -23,7 +23,7 @@ class Register extends React.Component {
     }
 
     onSubmitSignIn = () => {
-        fetch("https://face-recognition-api-pdqc.onrender.com/register", {
+        fetch("https://face-recognition-api-pdqc.onrender.com/register.json", {
             method: "post",
             headers: {'Content-Type': "application/json"},
             body: JSON.stringify({

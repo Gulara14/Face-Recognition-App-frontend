73ba57ac905e033aa2d86a353f62a9380eaf425f
Gulara Nuri <ramazanova.gulare@gmail.com>
2022-11-27 19:30:02 +0400

    last update

--- src/Components/Signin/Signin.js
+++ src/Components/Signin/Signin.js
@@ -20,7 +20,7 @@ class Signin extends React.Component {
     }
 
     onSubmitSignIn = () => {
-        fetch("http://localhost:3000/signin", {
+        fetch("hhttps://face-recognition-api-backend-production.up.railway.app/signin", {
             method: "post",
             headers: {'Content-Type': "application/json"},
             body: JSON.stringify({

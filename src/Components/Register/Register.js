9c7f807b0fc8663a4361dbe0505dd5fe29d4b92b
Gulara Nuri <ramazanova.gulare@gmail.com>
2022-11-27 13:02:26 +0400

    f

--- src/Components/Register/Register.js
+++ src/Components/Register/Register.js
@@ -23,7 +23,7 @@ class Register extends React.Component {
     }
 
     onSubmitSignIn = () => {
-        fetch("https://face-recognition-front.onrender.com/register", {
+        fetch("https://face-recognition-backend.onrender.com/register", {
             method: "post",
             headers: {'Content-Type': "application/json"},
             body: JSON.stringify({

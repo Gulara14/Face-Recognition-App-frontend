49bf379224796fafd8c6ffa209184fcdf9d891be
Gulara Nuri <ramazanova.gulare@gmail.com>
2022-11-27 13:11:28 +0400

    df

--- src/Components/Register/Register.js
+++ src/Components/Register/Register.js
@@ -23,7 +23,7 @@ class Register extends React.Component {
     }
 
     onSubmitSignIn = () => {
-        fetch("https://face-recognition-backend.onrender.com/register", {
+        fetch("https://face-recognition-front.onrender.com/register", {
             method: "post",
             headers: {'Content-Type': "application/json"},
             body: JSON.stringify({
@@ -40,7 +40,6 @@ class Register extends React.Component {
                     this.props.onRouteChange('home');
                 }
             })
-            .catch((error) => console.log(error));
     }
 
     render () {

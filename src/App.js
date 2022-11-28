3e868ad957cf46d1ba5dbced0b67ff1d2d4743b2
Gulara Nuri <ramazanova.gulare@gmail.com>
2022-11-28 10:11:11 +0400

    initial version

--- src/App.js
+++ src/App.js
@@ -65,7 +65,7 @@ class App extends Component {
 
   onPictureSubmit = () => {
     this.setState({imageUrl: this.state.input});
-      fetch("https://face-recognition-backend.onrender.com/imageurl", {
+      fetch("http://localhost:3000/imageurl", {
         method: "post",
         headers: {"Content-Type": "application/json"},
         body: JSON.stringify({
@@ -75,7 +75,7 @@ class App extends Component {
       .then(response => response.json())
       .then(response => {
         if (response) {
-          fetch("https://face-recognition-backend.onrender.com/image", {
+          fetch("http://localhost:3000/image", {
             method: "put",
             headers: {"Content-Type": "application/json"},
             body: JSON.stringify({
